56ed0816a73eed201fdb55496399fcaaf3baea3f
Steven Eucker <seucker0423@gmail.com>
2023-01-31 17:10:17 -0500

    formatting change and added reference to new Keywords class

--- SikuliSharp/AsyncTwoWayStreamsHandler.cs
+++ SikuliSharp/AsyncTwoWayStreamsHandler.cs
@@ -6,31 +6,25 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SikuliSharp
-{
-	public interface IAsyncDuplexStreamsHandlerFactory
-	{
+namespace SikuliSharp {
+	public interface IAsyncDuplexStreamsHandlerFactory {
 		IAsyncTwoWayStreamsHandler Create(TextReader stdout, TextReader stderr, TextWriter stdin);
 	}
 
-	public class AsyncDuplexStreamsHandlerFactory : IAsyncDuplexStreamsHandlerFactory
-	{
-		public IAsyncTwoWayStreamsHandler Create(TextReader stdout, TextReader stderr, TextWriter stdin)
-		{
+	public class AsyncDuplexStreamsHandlerFactory : IAsyncDuplexStreamsHandlerFactory {
+		public IAsyncTwoWayStreamsHandler Create(TextReader stdout, TextReader stderr, TextWriter stdin) {
 			return new AsyncTwoWayStreamsHandler(stdout, stderr, stdin);
 		}
 	}
 
-	public interface IAsyncTwoWayStreamsHandler : IDisposable
-	{
+	public interface IAsyncTwoWayStreamsHandler : IDisposable {
 		string ReadUntil(double timeoutInSeconds, params string[] expectedStrings);
 		IEnumerable<string> ReadUpToNow(double timeoutInSeconds);
 		void WriteLine(string command);
 		void WaitForExit();
 	}
 
-	public class AsyncTwoWayStreamsHandler : IAsyncTwoWayStreamsHandler
-	{
+	public class AsyncTwoWayStreamsHandler : IAsyncTwoWayStreamsHandler {
 		private readonly TextReader _stdout;
 		private readonly TextReader _stderr;
 		private readonly TextWriter _stdin;
@@ -38,8 +32,7 @@ namespace SikuliSharp
 		private readonly Task _readStdoutTask;
 		private readonly BlockingCollection<string> _pendingOutputLines = new BlockingCollection<string>();
 
-		public AsyncTwoWayStreamsHandler(TextReader stdout, TextReader stderr, TextWriter stdin)
-		{
+		public AsyncTwoWayStreamsHandler(TextReader stdout, TextReader stderr, TextWriter stdin) {
 			_stdout = stdout;
 			_stderr = stderr;
 			_stdin = stdin;
@@ -51,33 +44,26 @@ namespace SikuliSharp
 			_readStderrTask.Start();
 		}
 
-		public string ReadUntil(double timeoutInSeconds, params string[] expectedStrings)
-		{
-			while (true)
-			{
+		public string ReadUntil(double timeoutInSeconds, params string[] expectedStrings) {
+			while (true) {
 				string line;
-				if (timeoutInSeconds > 0)
-				{
+				if (timeoutInSeconds > 0) {
 					var timeout = TimeSpan.FromSeconds(timeoutInSeconds);
 					if (!_pendingOutputLines.TryTake(out line, timeout))
 						throw new TimeoutException(string.Format(@"No result in alloted time: {0:ss\.ffff}s", timeout));
 				}
-				else
-				{
+				else {
 					line = _pendingOutputLines.Take();
 				}
 
-				if (expectedStrings.Any(s => line.IndexOf(s, StringComparison.Ordinal) > -1))
-				{
+				if (expectedStrings.Any(s => line.IndexOf(s, StringComparison.Ordinal) > -1)) {
 					return line;
 				}
 			}
 		}
 
-		public IEnumerable<string> ReadUpToNow(double timeoutInSeconds)
-		{
-			while (true)
-			{
+		public IEnumerable<string> ReadUpToNow(double timeoutInSeconds) {
+			while (true) {
 				string line;
 				if (_pendingOutputLines.TryTake(out line, TimeSpan.FromSeconds(timeoutInSeconds)))
 					yield return line;
@@ -86,39 +72,35 @@ namespace SikuliSharp
 			}
 		}
 
-		public void WriteLine(string command)
-		{
+		public void WriteLine(string command) {
 			_stdin.WriteLine(command);
 		}
 
-		public void WaitForExit()
-		{
+		public void WaitForExit() {
 			_readStdoutTask.Wait();
 			_readStderrTask.Wait();
 		}
 
-		public void Dispose()
-		{
-			if (_readStdoutTask != null) _readStdoutTask.Dispose();
-			if (_readStderrTask != null) _readStderrTask.Dispose();
+		public void Dispose() {
+			if (_readStdoutTask != null)
+				_readStdoutTask.Dispose();
+			if (_readStderrTask != null)
+				_readStderrTask.Dispose();
 		}
 
-		private void ReadStdoutAsync()
-		{
+		private void ReadStdoutAsync() {
 			ReadStdAsync(_stdout);
 		}
 
-		private void ReadStderrAsync()
-		{
-			ReadStdAsync(_stderr, "[error] ");
+		private void ReadStderrAsync() {
+			ReadStdAsync(_stderr, $"{KeyWords.ErrorMarker} ");
 		}
 
-		private void ReadStdAsync(TextReader output, string prefix = null)
-		{
+		private void ReadStdAsync(TextReader output, string prefix = null) {
 			string line;
-			while ((line = output.ReadLine()) != null)
-			{
-				if (prefix != null) line = prefix + line;
+			while ((line = output.ReadLine()) != null) {
+				if (prefix != null)
+					line = prefix + line;
 #if (DEBUG)
 				Debug.WriteLine("SIKULI> " + line);
 #endif
