9becfe216d3d20c7449febea8d6c72590f5b183b
Steven Eucker <seucker0423@gmail.com>
2023-01-31 17:13:16 -0500

    formatting. Added regex to take string and createIRegion object

--- SikuliSharp/Region.cs
+++ SikuliSharp/Region.cs
@@ -1,161 +1,141 @@
-﻿namespace SikuliSharp
-{
-	public class Region : IRegion
-	{
+﻿using System.Text.RegularExpressions;
+
+namespace SikuliSharp {
+	public class Region : IRegion {
+		private static readonly Regex RegionRegex = new Regex(@"R\[(\d*),\s{0,}(\d*) (\d*)x(\d*)\]", RegexOptions.Compiled);
 		private int _x, _y, _w, _h;
 
-		public void Validate()
-		{
+		public void Validate() {
 		}
 
-		public string ToSikuliScript()
-		{
+		public string ToSikuliScript() {
 			return $"Region({_x}, {_y}, {_w}, {_h})";
 		}
 
-		public Region()
-		{
+		public Region() {
 
 		}
 
-		public Region(Region region)
-		{
+		public Region(Region region) {
 			_x = region.GetX();
 			_y = region.GetY();
 			_w = region.GetW();
 			_h = region.GetH();
 		}
 
-		public Region(int x, int y, int w, int h)
-		{
+		public Region(int x, int y, int w, int h) {
 			_x = x;
 			_y = y;
 			_w = w;
 			_h = h;
 		}
 
-		public void SetX(int x)
-		{
+		public Region(string regionString) {
+			var regionMatchs = RegionRegex.Match(regionString);
+			_x = int.Parse(regionMatchs.Groups[1].ToString());
+			_y = int.Parse(regionMatchs.Groups[2].ToString());
+			_w = int.Parse(regionMatchs.Groups[3].ToString());
+			_h = int.Parse(regionMatchs.Groups[4].ToString());
+		}
+
+		public void SetX(int x) {
 			_x = x;
 		}
 
-		public void SetY(int y)
-		{
+		public void SetY(int y) {
 			_y = y;
 		}
 
-		public void SetW(int w)
-		{
+		public void SetW(int w) {
 			_w = w;
 		}
 
-		public void SetH(int h)
-		{
+		public void SetH(int h) {
 			_h = h;
 		}
 
-		public void MoveTo(int x, int y)
-		{
+		public void MoveTo(int x, int y) {
 			_x = x;
 			_y = y;
 		}
 
-		public void SetROI(int x, int y, int w, int h)
-		{
+		public void SetROI(int x, int y, int w, int h) {
 			_x = x;
 			_y = y;
 			_w = w;
 			_h = h;
 		}
 
-		public int GetX()
-		{
+		public int GetX() {
 			return _x;
 		}
 
-		public int GetY()
-		{
+		public int GetY() {
 			return _y;
 		}
 
-		public int GetW()
-		{
+		public int GetW() {
 			return _w;
 		}
 
-		public int GetH()
-		{
+		public int GetH() {
 			return _h;
 		}
 
-		public Location GetTopLeft()
-		{
+		public Location GetTopLeft() {
 			return new Location(_x, _y);
 		}
 
-		public Location GetTopRight()
-		{
+		public Location GetTopRight() {
 			return new Location(_x + _w - 1, _y);
 		}
 
-		public Location GetBottomLeft()
-		{
+		public Location GetBottomLeft() {
 			return new Location(_x, _y + _h - 1);
 		}
 
-		public Location GetBottomRight()
-		{
+		public Location GetBottomRight() {
 			return new Location(_x + _w - 1, _y + _h - 1);
 		}
 
-		public Region Offset(int x, int y)
-		{
+		public Region Offset(int x, int y) {
 			return new Region(_x + x, _y + y, _w, _h);
 		}
 
-		public Region Offset(Point point)
-		{
+		public Region Offset(Point point) {
 			return new Region(_x + point.X, _y + point.Y, _w, _h);
 		}
 
-		public Region Grow(int range)
-		{
+		public Region Grow(int range) {
 			return new Region(_x - range, _y - range, _w + range, _h + range);
 		}
 
-		public Region Grow(int width, int height)
-		{
+		public Region Grow(int width, int height) {
 			return new Region(_x - width, _y - height, _w + width, _h + height);
 		}
 
-		public Region Grow(int left, int right, int top, int bottom)
-		{
+		public Region Grow(int left, int right, int top, int bottom) {
 			return new Region(_x - left, _y - top, _w + right, _h + bottom);
 		}
 
-		public Region Above(int range)
-		{
+		public Region Above(int range) {
 			return new Region(_x, _y - range, _w, range);
 		}
 
-		public Region Below(int range)
-		{
+		public Region Below(int range) {
 			return new Region(_x, _y + _h, _w, range);
 		}
 
-		public Region Left(int range)
-		{
+		public Region Left(int range) {
 			return new Region(_x - range, _y, range, _w);
 		}
 
-		public Region Right(int range)
-		{
+		public Region Right(int range) {
 			return new Region(_x + _w, _y, _h, range);
 		}
 	}
 
-	public interface IRegion
-	{
+	public interface IRegion {
 		void Validate();
 		string ToSikuliScript();
 	}
