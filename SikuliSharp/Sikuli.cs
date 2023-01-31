26daff3833de9469e64038722de0d76749710880
Steven Eucker <seucker0423@gmail.com>
2023-01-31 17:13:56 -0500

    formatting. added function to create a v2 session

--- SikuliSharp/Sikuli.cs
+++ SikuliSharp/Sikuli.cs
@@ -1,39 +1,37 @@
 ﻿using System;
 using System.IO;
 
-namespace SikuliSharp
-{
-	public static class Sikuli
-	{
-		public static ISikuliSession CreateSession()
-		{
+namespace SikuliSharp {
+	public static class Sikuli {
+		public static ISikuliSession CreateSession() {
 			return new SikuliSession(CreateRuntime());
 		}
 
-		public static SikuliRuntime CreateRuntime()
-		{
+		public static ISikuliSessionV2 CreateSessionV2() {
+			return new SikuliSessionV2(CreateRuntime());
+		}
+
+		public static SikuliRuntime CreateRuntime() {
 			return new SikuliRuntime(
 				new AsyncDuplexStreamsHandlerFactory(),
 				new SikuliScriptProcessFactory()
 				);
 		}
 
-		public static string RunProject(string projectPath)
-		{
+		public static string RunProject(string projectPath) {
 			return RunProject(projectPath, null);
 		}
 
-		public static string RunProject(string projectPath, string args)
-		{
-			if (projectPath == null) throw new ArgumentNullException("projectPath");
+		public static string RunProject(string projectPath, string args) {
+			if (projectPath == null)
+				throw new ArgumentNullException("projectPath");
 
 			if (!Directory.Exists(projectPath))
 				throw new DirectoryNotFoundException(string.Format("Project not found in path '{0}'", projectPath));
 
 			var processFactory = new SikuliScriptProcessFactory();
 			var version = processFactory.GetSikuliVersion().WithProject(projectPath, args);
-			using (var process = processFactory.Start(version))
-			{
+			using (var process = processFactory.Start(version)) {
 				var output = process.StandardOutput.ReadToEnd();
 				process.WaitForExit();
 				return output;
