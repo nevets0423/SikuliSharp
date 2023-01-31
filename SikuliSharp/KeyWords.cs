6821505faab336baa9ab4e606eb1c5f1bddacdb7
Steven Eucker <seucker0423@gmail.com>
2023-01-31 17:09:37 -0500

    added a class to hold strings used in multiple places to help lower possible errors

---
+++ SikuliSharp/KeyWords.cs
@@ -0,0 +1,10 @@
+﻿namespace SikuliSharp {
+	public static class KeyWords {
+		public static readonly string ObserverPrefix = "SIKULI#OBSERVER#:";
+		public static readonly string ErrorMarker = "[Error]";
+		public static readonly string ReturnIdentifier = "SIKULI#:";
+		public static readonly string None = "None";
+		public static readonly string MatchTextRegex = @"M\[.*?msec\]";
+		public static readonly string PatternTextRegex = @"P\(.*\) S: (.*)";
+	}
+}
